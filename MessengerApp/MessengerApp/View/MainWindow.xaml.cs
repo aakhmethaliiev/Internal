61103bc0943d77f8a7c462ef2e7db51e47d5b608
Akhmethaliiev <AAkhmethaliiev@kiev.luxoft.com>
2019-10-15 14:51:13 +0300

    Created new MessegerApp

---
+++ MessengerApp/MessengerApp/View/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace MessengerApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MessengerApp/MessengerApp/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.ObjectModel;
+using Caliburn.Micro;
+using Contracts;
+
+namespace MessengerApp.ViewModel
+{
+    public class MainWindowViewModel: PropertyChangedBase
+    {
+        public MainWindowViewModel()
+        {
+            MenuItems = new ObservableCollection<ShellMenuItem>();
+        }
+
+        public ObservableCollection<ShellMenuItem> MenuItems { get; private set; }
+
+        private ShellMenuItem _selectedMenuItem;
+        public ShellMenuItem SelectedMenuItem
+        {
+            get => _selectedMenuItem;
+            set
+            {
+                if (_selectedMenuItem == value)
+                    return;
+                _selectedMenuItem = value;
+                NotifyOfPropertyChange(() => SelectedMenuItem);
+                NotifyOfPropertyChange(() => CurrentView);
+            }
+        }
+
+        public object CurrentView => _selectedMenuItem?.ScreenViewModel;
+    }
+}
