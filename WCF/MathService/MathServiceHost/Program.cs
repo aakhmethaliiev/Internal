7cb50432dc5173bb4de2a4a05dbec619abe16ddc
Akhmethaliiev <AAkhmethaliiev@kiev.luxoft.com>
2019-10-23 16:01:22 +0300

    Finished test project for wcf

---
+++ WCF/MathService/MathServiceHost/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace MathServiceHost
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            var host = new ServiceHost(typeof(MathService.MathService));
+            host.Open();
+            Console.WriteLine("Service Hosted Successfully");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ WCF/WindowsFormsClient/WindowsFormsClient/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+using WindowsFormsClient.ServiceReference1;
+
+namespace WindowsFormsClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var _obj = new MathServiceClient("NetTcpBinding_IMathService");
+            label4.Text = _obj.Addition(Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text)).ToString();
+        }
+    }
+}
